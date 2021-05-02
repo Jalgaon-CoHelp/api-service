bc96ae785475a6f1beafefb4a4185bc4bbaea582
Shreyas <shreyaspatilg@gmail.com>
2021-05-02 16:45:27 +0530

    Handle error in repository

--- port/db/src/main/kotlin/in/jalgaoncohelp/db/hospitals/HospitalRepositoryImpl.kt
+++ port/db/src/main/kotlin/in/jalgaoncohelp/db/hospitals/HospitalRepositoryImpl.kt
@@ -40,40 +40,44 @@ HospitalRepositoryImpl(
 ) : HospitalRepository {
 
     override suspend fun addHospital(hospitalParams: NewHospitalParams) = withContext(ioContext) {
-        db.hospitalsQueries.addHospital(
-            name = hospitalParams.name,
-            address = hospitalParams.address,
-            taluka_id = hospitalParams.talukaId,
-            contact_1 = hospitalParams.contact1,
-            contact_2 = hospitalParams.contact2,
-            bed_general = hospitalParams.beds.general,
-            bed_icu = hospitalParams.beds.icu,
-            bed_oxygen = hospitalParams.beds.oxygen,
-            bed_ventilator = hospitalParams.beds.ventilator
-        )
+        runCatching {
+            db.hospitalsQueries.addHospital(
+                name = hospitalParams.name,
+                address = hospitalParams.address,
+                taluka_id = hospitalParams.talukaId,
+                contact_1 = hospitalParams.contact1,
+                contact_2 = hospitalParams.contact2,
+                bed_general = hospitalParams.beds.general,
+                bed_icu = hospitalParams.beds.icu,
+                bed_oxygen = hospitalParams.beds.oxygen,
+                bed_ventilator = hospitalParams.beds.ventilator
+            )
+        }.getOrElse { error("Failed to add hospital") }
     }
 
     override suspend fun getRecentlyUpdatedHospitals(page: Page): List<Hospital> = withContext(ioContext) {
-        db.hospitalsQueries.getRecentlyUpdatedHospitals(page.limit, page.offset)
-            .executeAsList()
-            .map {
-                Hospital(
-                    id = it.id,
-                    name = it.name,
-                    address = it.address,
-                    taluka = Taluka(it.taluka_id, it.taluka_name),
-                    contact1 = it.contact_1,
-                    contact2 = it.contact_2,
-                    beds = Beds(
-                        general = it.bed_general,
-                        oxygen = it.bed_oxygen,
-                        icu = it.bed_icu,
-                        ventilator = it.bed_ventilator
-                    ),
-                    createdAt = it.created_at,
-                    updatedAt = it.updated_at
-                )
-            }
+        runCatching {
+            db.hospitalsQueries.getRecentlyUpdatedHospitals(page.limit, page.offset)
+                .executeAsList()
+                .map {
+                    Hospital(
+                        id = it.id,
+                        name = it.name,
+                        address = it.address,
+                        taluka = Taluka(it.taluka_id, it.taluka_name),
+                        contact1 = it.contact_1,
+                        contact2 = it.contact_2,
+                        beds = Beds(
+                            general = it.bed_general,
+                            oxygen = it.bed_oxygen,
+                            icu = it.bed_icu,
+                            ventilator = it.bed_ventilator
+                        ),
+                        createdAt = it.created_at,
+                        updatedAt = it.updated_at
+                    )
+                }
+        }.getOrElse { error("Failed to load hospitals") }
     }
 
     override suspend fun getTotalHospitalsCount(): Long {
