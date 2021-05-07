eb0fae0c2a6435aa88e1ee33b67001c95d28cde5
Shreyas <shreyaspatilg@gmail.com>
2021-05-07 10:09:17 +0530

    Refactor: Change resource col names

--- migration/resources/sql/2021-05-06-01-create-resources.sql
+++ migration/resources/sql/2021-05-06-01-create-resources.sql
@@ -1,9 +1,10 @@
 CREATE TABLE resources
 (
     id            SERIAL8 PRIMARY KEY,
+    type          VARCHAR(50) NOT NULL,
     contact_name  VARCHAR     NOT NULL,
     mobile_number VARCHAR(15) NOT NULL,
-    resource_type VARCHAR(50) NOT NULL,
+    resource_name VARCHAR(50) NOT NULL,
     address       VARCHAR,
     taluka_id     INT         NOT NULL REFERENCES talukas (id),
     note          VARCHAR,
