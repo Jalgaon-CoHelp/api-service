eb0490df4a420b4c02be4b2e4bc16c87fbe99a95
Shreyas <shreyaspatilg@gmail.com>
2021-05-07 10:11:52 +0530

    Refactor: new resource params

--- core/src/in/jalgaoncohelp/core/resource/model/NewResourceParams.kt
+++ core/src/in/jalgaoncohelp/core/resource/model/NewResourceParams.kt
@@ -1,9 +1,10 @@
 package `in`.jalgaoncohelp.core.resource.model
 
 data class NewResourceParams(
+    val type: ResourceType,
     val contactName: String,
     val mobileNumber: String,
-    val type: ResourceType,
+    val resourceName: ResourceName,
     val address: String?,
     val talukaId: Int,
     val note: String?
