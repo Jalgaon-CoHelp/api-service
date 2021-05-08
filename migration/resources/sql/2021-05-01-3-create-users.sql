97605da9ee3413a0b1e4be4d87338419f18481f4
Shreyas <shreyaspatilg@gmail.com>
2021-05-08 11:56:09 +0530

    Keep email column unique in users schema

--- migration/resources/sql/2021-05-01-3-create-users.sql
+++ migration/resources/sql/2021-05-01-3-create-users.sql
@@ -3,10 +3,10 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users
 (
     id         BIGSERIAL PRIMARY KEY,
-    name       VARCHAR     NOT NULL,
-    email      VARCHAR     NOT NULL,
-    phone      VARCHAR(15) NOT NULL,
-    password   VARCHAR     NOT NULL,
-    taluka_id  INT         NOT NULL REFERENCES talukas (id),
-    created_at TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP
+    name       VARCHAR        NOT NULL,
+    email      VARCHAR UNIQUE NOT NULL,
+    phone      VARCHAR(15)    NOT NULL,
+    password   VARCHAR        NOT NULL,
+    taluka_id  INT            NOT NULL REFERENCES talukas (id),
+    created_at TIMESTAMPTZ    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
