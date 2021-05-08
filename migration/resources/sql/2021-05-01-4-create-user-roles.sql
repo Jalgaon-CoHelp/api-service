1144a4bd69239981fa87aeeb3743c2b599391193
Shreyas <shreyaspatilg@gmail.com>
2021-05-08 13:54:23 +0530

    Fix user roles migrations: provide on-delete cascade-ability

--- migration/resources/sql/2021-05-01-4-create-user-roles.sql
+++ migration/resources/sql/2021-05-01-4-create-user-roles.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user_roles
 (
     id      BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users (id),
-    role_id INT    NOT NULL REFERENCES roles (id)
+    user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    role_id INT    NOT NULL REFERENCES roles (id) ON DELETE CASCADE
 );
