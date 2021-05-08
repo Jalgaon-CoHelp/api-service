28e35db71aad25bd8ecd1e294c6c05c48f0d5ec1
Shreyas <shreyaspatilg@gmail.com>
2021-05-08 11:49:19 +0530

    Add and implement methods in user repository

--- core/src/in/jalgaoncohelp/core/user/UserRepository.kt
+++ core/src/in/jalgaoncohelp/core/user/UserRepository.kt
@@ -23,10 +23,13 @@
  */
 package `in`.jalgaoncohelp.core.user
 
+import `in`.jalgaoncohelp.core.user.model.User
+
 interface UserRepository {
     suspend fun isEmailAlreadyUsed(email: String): Boolean
     suspend fun findUserById(id: Long): User
-    suspend fun findUserByEmail(email: String): Long
+    suspend fun findUserByEmail(email: String): User
     suspend fun findUserByEmailAndPassword(email: String, password: String): User
     suspend fun addUser(name: String, email: String, phone: String, password: String, talukaId: Int)
+    suspend fun findUserIdByEmail(email: String): Long
 }
--- port/db/src/main/kotlin/in/jalgaoncohelp/db/user/UserRepositoryImpl.kt
+++ port/db/src/main/kotlin/in/jalgaoncohelp/db/user/UserRepositoryImpl.kt
@@ -25,8 +25,8 @@ package `in`.jalgaoncohelp.db.user
 
 import `in`.jalgaoncohelp.core.exception.notFoundError
 import `in`.jalgaoncohelp.core.roles.Role
-import `in`.jalgaoncohelp.core.user.User
 import `in`.jalgaoncohelp.core.user.UserRepository
+import `in`.jalgaoncohelp.core.user.model.User
 import `in`.jalgaoncohelp.db.JalgaonCoHelpDatabase
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.withContext
@@ -68,9 +68,21 @@ class UserRepositoryImpl(
             } ?: notFoundError("User not exists")
     }
 
-    override suspend fun findUserByEmail(email: String): Long = withContext(ioContext) {
-        return@withContext db.usersQueries.findUserByEmail(email).executeAsOneOrNull()
-            ?: notFoundError("User not exists")
+    override suspend fun findUserIdByEmail(email: String): Long = withContext(ioContext) {
+        db.usersQueries.findUserIdByEmail(email).executeAsOneOrNull() ?: notFoundError("User not found")
+    }
+
+    override suspend fun findUserByEmail(email: String): User = withContext(ioContext) {
+        db.usersQueries.findUserByEmail(email).executeAsOneOrNull()?.let {
+            User(
+                id = it.user_id,
+                name = it.name,
+                email = it.email,
+                phone = it.phone,
+                taluka = it.taluka_name,
+                role = Role.findByName(it.role)
+            )
+        } ?: notFoundError("User not found")
     }
 
     override suspend fun findUserByEmailAndPassword(email: String, password: String): User = withContext(ioContext) {
