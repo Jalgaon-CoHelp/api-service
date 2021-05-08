f7171a762e007844f3accfe5cc2a82f1f98545e5
Shreyas <shreyaspatilg@gmail.com>
2021-05-08 13:02:25 +0530

    Refactor user permissions: Introduce hospital class

--- api/src/in/jalgaoncohelp/api/authentication/UserPermissions.kt
+++ api/src/in/jalgaoncohelp/api/authentication/UserPermissions.kt
@@ -27,9 +27,13 @@ import `in`.jalgaoncohelp.core.roles.Role
 import `in`.jalgaoncohelp.core.user.model.User
 
 sealed class UserPermissions(val permittedRoles: List<Role>) {
-    object CreateNewHospital : UserPermissions(listOf(Role.Admin))
-    object UpdateHospital : UserPermissions(listOf(Role.Admin, Role.Volunteer))
-    object DeleteHospital : UserPermissions(listOf(Role.Admin))
+    sealed class Hospital(permittedRoles: List<Role>) : UserPermissions(permittedRoles) {
+        object CreateNew : UserPermissions(listOf(Role.Admin))
+        object UpdateInfo : UserPermissions(listOf(Role.Admin))
+        object UpdateBeds : UserPermissions(listOf(Role.Admin, Role.Volunteer))
+        object Delete : UserPermissions(listOf(Role.Admin))
+    }
+
 }
 
 infix fun User.hasPermission(permissions: UserPermissions): Boolean {
